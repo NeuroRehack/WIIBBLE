2cc1be32b0253afe43e117ae0c70898407562ffa
SamiKaab <sami.kaab.alain@gmail.com>
2024-08-27 16:04:56 +1000

    Remove unused C# compile commands, improve compile commands

--- compiler.bat
+++ compiler.bat
@@ -1,17 +1,11 @@
+@REM build c# project and move content of \bin\Debug\net8.0
+cd WiiBalanceBoardLibrary
+dotnet build
+cd ..
 if not exist outputBuild\ mkdir outputBuild\
-@REM if not exist outputBuild\WiiBalanceBoardConnection\ mkdir outputBuild\WiiBalanceBoardConnection\
 call venv\Scripts\activate.bat
-pyinstaller --clean --onefile --noconsole scale.py --icon=images\\logo.ico --name=WIIBBLE --hidden-import=hid --add-data="images;images" --add-data="WiiBalanceBoardConnection;WiiBalanceBoardConnection"
+pyinstaller --clean --onefile --noconsole main.py --icon=images\\logo.ico --name=WIIBBLE --hidden-import=hid --add-data="images;images" --add-data="WiiBalanceBoardLibrary;WiiBalanceBoardLibrary"
 move dist\\WIIBBLE.exe outputBuild\\WIIBBLE.exe
 rmdir /s /q build
 del /f /q WIIBBLE.spec
 rmdir /s /q dist
-@REM @REM build c# project and move content of \bin\Debug\net8.0
-@REM cd WiiBalanceBoardConnection
-@REM dotnet build
-@REM cd bin\Debug\net8.0
-@REM @REM create the outputBuild folder if it does not exist
-@REM copy *.* ..\..\..\..\outputBuild\WiiBalanceBoardConnection\
-@REM cd ..\..\..\..
-@REM @REM copy the images folder and its content to the outputBuild folder
-@REM @REM xcopy images outputBuild\images\ /E /Y
